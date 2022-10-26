063808c5e2b2463f5da5e487ce88c80f5125e149
BigSev15 <palat008@umn.edu>
2022-10-26 13:46:47 -0500

    created decision tree for licensing badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === 'MIT') {
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    } else if (license === 'APACHE 2.0') {
+        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    } else if (license === 'GPL 3.0') {
+        return '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)'
+    } else if (license === 'BSD 3') {
+        return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
